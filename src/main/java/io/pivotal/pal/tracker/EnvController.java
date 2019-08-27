83266e26d5c26a19cd80ea0c0b70908a1b3f93f4
ashish8024571 <ashish.jain5@rbs.co.uk>
2019-08-27 12:26:39 +0530

    test

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,33 +1,34 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class EnvController {
-
     private String port;
     private String memoryLimit;
     private String cfInstanceIndiex;
     private String cfInstanceAddr;
-
-
-    public EnvController(@Value("${port:8080}")String port,
-                         @Value("${memoryLimit:1024}")String memoryLimit,
-                         @Value("${cfInstanceIndiex:20}")String cfInstanceIndiex,
-                         @Value("${cfInstanceAddr:2}")String cfInstanceAddr ) {
+    public EnvController(@Value("${port:NOT SET}")String port,
+                         @Value("${memoryLimit:NOT SET}")String memoryLimit,
+                         @Value("${cfInstanceIndiex:NOT SET}")String cfInstanceIndiex,
+                         @Value("${cfInstanceAddr:NOT SET}")String cfInstanceAddr ) {
         this.port= port;
         this.memoryLimit=memoryLimit;
         this.cfInstanceIndiex=cfInstanceIndiex;
         this.cfInstanceAddr=cfInstanceAddr;
     }
-
     @GetMapping("/env")
-    public String getEnv(){
-
-        return "";
-
+    public Map<String, String> getEnv(){
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("PORT",this.port);
+        map.put("MEMORY_LIMIT", this.memoryLimit);
+        map.put("CF_INSTANCE_INDEX", this.cfInstanceIndiex);
+        map.put("CF_INSTANCE_ADDR", this.cfInstanceAddr);
+        return map;
     }
-
-
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    private String hello;
+
+    public WelcomeController( @Value("${welcome.message}") String hello) {
+        this.hello = hello;
+    }
+
+    @RequestMapping ("/")
+    public String sayHello(){
+
+        return hello;
+    }
 }
