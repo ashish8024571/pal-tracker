51bc0faf765737e7fb648f0508897028b9f9a700
ashish8024571 <ashish.jain5@rbs.co.uk>
2019-08-28 10:10:51 +0530

    API

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,62 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+@Component
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+
+    Map<Long,TimeEntry> map = new HashMap<>();
+    AtomicLong idObject = new AtomicLong();
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        long id = idObject.incrementAndGet();
+        TimeEntry timEntry = new TimeEntry(id,timeEntry.getProjectId(), timeEntry.getUserId(),timeEntry.getDate(),timeEntry.getHours());
+        map.put(id,timEntry);
+       return timEntry;
+    }
+
+    @Override
+    public TimeEntry find(long timeEntryId) {
+        return map.get(timeEntryId);
+
+    }
+
+    @Override
+    public void delete(long id) {
+        map.remove(id);
+
+    }
+
+    @Override
+    public List list() {
+        List<TimeEntry> list = map.values().stream()
+                .collect(Collectors.toList());
+        return list;
+    }
+
+    @Override
+    public TimeEntry update(long l, TimeEntry timeEntry) {
+        TimeEntry value= map.get(l);
+        TimeEntry newTimeEntry= null;
+        if (value != null){
+            newTimeEntry = new TimeEntry(l,timeEntry.getProjectId(), timeEntry.getUserId(),timeEntry.getDate(),timeEntry.getHours());
+            map.replace(l,newTimeEntry);
+
+        }
+
+        return newTimeEntry;
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,68 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.sql.Time;
+import java.util.List;
+
+@RestController
+public class TimeEntryController {
+
+
+
+
+    private TimeEntryRepository rep ;
+
+
+
+    public TimeEntryController(  TimeEntryRepository rep) {
+
+        this.rep = rep;
+    }
+
+    @RequestMapping(path = "/time-entries" , method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity create(  @RequestBody TimeEntry timeEntryToCreate) {
+       TimeEntry timeEntry = rep.create(timeEntryToCreate);
+        return new ResponseEntity(timeEntry,HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "/time-entries/{id}" , method = RequestMethod.GET)
+
+    @ResponseBody
+    public ResponseEntity<TimeEntry> read(@PathVariable("id") long id) {
+        TimeEntry timeEntry = rep.find(id);
+        if(timeEntry==null)
+            return new ResponseEntity(timeEntry,HttpStatus.NOT_FOUND);
+        return new ResponseEntity(timeEntry,HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/time-entries" , method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> list = rep.list();
+        return new ResponseEntity(list,HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/time-entries/{timeEntryId}" , method = RequestMethod.PUT)
+    public ResponseEntity update(@PathVariable("timeEntryId")long timeEntryId, @RequestBody TimeEntry expected) {
+        TimeEntry timeEntry = rep.update(timeEntryId,expected);
+        if(timeEntry==null)
+            return new ResponseEntity(timeEntry,HttpStatus.NOT_FOUND);
+        return new ResponseEntity(timeEntry,HttpStatus.OK);
+    }
+
+
+    @RequestMapping(path = "/time-entries/{timeEntryId}" , method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable("timeEntryId")long timeEntryId) {
+
+        rep.delete(timeEntryId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
